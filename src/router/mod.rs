9db1cc833cb86b15fef10f5dc6e443c2c8107eb3
ucalan <1025997470@qq.com>
2022-10-02 00:28:37 +0800

    美化代码

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -19,7 +19,8 @@ pub struct Param {
 #[post("/hello")]
 #[instrument(name = "hello-request")]
 pub async fn hello(req: web::Query<Param>) -> impl Responder {
-    let _ = try_do(|| async {
+    // 业务逻辑
+    try_do(|| async {
         active_model!(users {
             phone: req.into_inner().phone,
         })
@@ -27,8 +28,16 @@ pub async fn hello(req: web::Query<Param>) -> impl Responder {
         .await
         .inspect_err(|err| log::error!("{}", err.to_string()))
     })
-    .await;
-    HttpResponse::Ok().body("Hello world!")
+    .await
+    // 成功逻辑
+    .map(|_| HttpResponse::Ok().body("Ok"))
+    // 失败逻辑
+    .unwrap_or_else(|err| {
+        // 打个失败日志
+        log::error!("{}", err.to_string());
+        // 返回错误码，不能把数据库的错误信息暴露给用户
+        HttpResponse::InternalServerError().body("Failed")
+    })
 }
 
 /// Use this function to initialize routers
