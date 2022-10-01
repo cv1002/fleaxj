ddeca1ec8c092d0624fcf2ad28e197d1735bd797
ucalan <1025997470@qq.com>
2022-10-02 02:21:55 +0800

    只有util才需要doctest、其他的用unittest

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,5 @@
 #![allow(dead_code)]
 #![allow(unstable_name_collisions)]
 
-/* Declare the mods */
-pub mod conf;
-pub mod database;
-pub mod router;
-pub mod serve;
+/* Declare the mods that should use doctest */
 pub mod util;
