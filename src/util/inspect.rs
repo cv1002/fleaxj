81bfa575271dbe65207162ffcb92cb7cd53a1a99
ucalan <1025997470@qq.com>
2022-10-02 02:47:29 +0800

    修改注释

--- src/util/inspect.rs
+++ src/util/inspect.rs
@@ -1,33 +1,34 @@
 /* Trait declaration */
 pub trait ResultInspect<F: FnOnce(&T), T: Sized> {
+    /// Call function when ok.
     fn inspect_ok(self, f: F) -> Self;
 }
-
 pub trait ResultInspectRef<F: FnOnce(&T), T: Sized> {
+    /// Call function when ok.
     fn inspect_ref(&self, f: F);
 }
-
 pub trait ResultInspectErr<F: FnOnce(&E), E: Sized> {
+    /// Call function when error.
     fn inspect_error(self, f: F) -> Self;
 }
-
 pub trait ResultInspectErrRef<F: FnOnce(&E), E: Sized> {
+    /// Call function when error, but doesn't move.
     fn inspect_err_ref(&self, f: F);
 }
-
 pub trait OptionInspect<F: FnOnce(&T), T: Sized> {
+    /// Call function when some.
     fn inspect_some(self, f: F) -> Self;
 }
-
 pub trait OptionInspectRef<F: FnOnce(&T), T: Sized> {
+    /// Call function when some, but doesn't move.
     fn inspect_ref(&self, f: F);
 }
-
 pub trait OptionInspectNone<F: FnOnce()> {
+    /// Call function when none.
     fn inspect_none(self, f: F) -> Self;
 }
-
 pub trait OptionInspectNoneRef<F: FnOnce()> {
+    /// Call function when none, but doesn't move.
     fn inspect_none_ref(&self, f: F);
 }
 
